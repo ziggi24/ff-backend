a14f1aaab3144972afaee9044055b8615aaf65df
Zach Sharpe <ziggi24@gmail.com>
2021-01-05 21:38:57 -0800

    added mongo-sanitize to prevent nosql query injection

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
 const controllers = require('./controllers');
+const expressMongoSanitize = require('express-mongo-sanitize');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan('dev'));
+app.use(expressMongoSanitize());
 app.use(express.json());
 
 app.get('/', (req, res) => { // defualt route to be replaced with a static homepage with instructions on how to use
